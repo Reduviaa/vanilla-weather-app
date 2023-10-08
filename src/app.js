357cdefedcb0720498721b8dd25da862cd17d3b4
Reduviaa <142176851+Reduviaa@users.noreply.github.com>
2023-10-08 19:07:10 +0100

    updated css

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 let now = new Date();
 let date = now.getDate();
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let year = now.getFullYear();
 
 let days = [
